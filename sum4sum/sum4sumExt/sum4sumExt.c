ded84113c1ad1b75b7cf241faf0b2f692f682224
Michael Torres <michael.46@live.it>
2025-11-14 11:48:40 +0100

    sum4sum ext

---
+++ sum4sum/sum4sumExt/sum4sumExt.c
@@ -0,0 +1,19 @@
+//
+// Created by Michael Torres on 14.11.25.
+//
+
+//
+// Created by Michael Torres on 13.11.25.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 35;
+    int b = 10;
+    int c = a + b;  //c var is local variable, important for understand "C"
+
+    printf(" %d is sum4sum \n", c);
+    return 0;
+}
